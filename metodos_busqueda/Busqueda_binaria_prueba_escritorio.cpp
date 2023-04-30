0c916d32f22d90d6c501474b289f02e427727f7b
David Freddy <111796950+frd2377@users.noreply.github.com>
2023-04-29 19:48:09 -0500

    Update Busqueda_binaria_prueba_escritorio.cpp

--- metodos_busqueda/Busqueda_binaria_prueba_escritorio.cpp
+++ metodos_busqueda/Busqueda_binaria_prueba_escritorio.cpp
@@ -4,39 +4,38 @@ using namespace std;
 int main(){
 	int valorBuscado = 30;
 	int Y,M,Z;
-	int lista[] = {10,20,30,40,50,60,70,80};
+	int lista[] = {10,20,30,40,50,60,70,80,90,100,110};
 	Y = 0;
 	Z = (sizeof(lista)/sizeof(lista[0]))-1;
-	M = (Y+Z)/2;
-	cout<<"Busqueda binaria: \n"<<endl;
-	cout<<"--Prueba de escritorio--"<<endl;
-	cout<<"Y\tZ\tM\t"<<endl;
-	cout<<Y<<"\t"<<Z<<"\t"<<Z<<"\t";
-	for(int i= Y;i<Z;i++){
+	cout<<"Lista: [ ";
+	for(int i= Y;i<Z+1;i++){
 		cout<<lista[i]<<" ";
 	}
-	cout<<endl;
+	cout<<" ]\n";
+	cout<<"Y = "<<Y<<" Z = "<<Z<<endl;
+	cout<<"Y\tM\tZ\t"<<endl;
+	
 	while(M!=Y && M!=Z){
+		M = (Y+Z)/2;
+		
 		if(lista[M] > valorBuscado){
-			Z = M-1;
-			M = (Y+Z)/2;
+			Z = M-1;	
 		}else if(lista[M] < valorBuscado){
-			Y = M +1;
-			M = (Y+Z)/2;	
+			Y = M +1;	
 		}
-		cout<<Y<<"\t"<<Z<<"\t"<<M<<"\t";
-		for(int i= Y;i<Z+1;i++){
+		else if(lista[M] == valorBuscado){
+			Z=M;
+			Y=M;
+			cout<<"\t\t\t"<<lista[M]<<endl;
+		}
+		cout<<Y<<"\t"<<M<<"\t"<<Z<<"\t";
+		for(int i= Y;i<M+1;i++){
 			cout<<lista[i]<<" ";
 		}
-		cout<<endl;
-		
+		cout<<endl;	
 	}
-	cout<<"Resultado: "<<endl; 
-	cout<<"Y\t"<<"M\t"<<"Z\t"<<endl;
-	cout<<Y<<"\t"<<M<<"\t"<<Z<<"\t\n"<<endl;
-	cout<<"lista[Y]:\t"<<"lista[M]:\t"<<"lista[Z]:\t"<<endl;
-	cout<<lista[Y]<<"\t\t"<<lista[M]<<"\t\t"<<lista[Z]<<"\t"<<endl;
 	return 0;
 }
+
 
 
